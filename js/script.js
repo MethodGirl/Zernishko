808be158443fad3c6d3ce9eaa5822829f713e3c3
MethodGirl <94073698+MethodGirl@users.noreply.github.com>
2026-02-28 02:13:55 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menuSwitchers = document.querySelectorAll('.switcher');
+
+let drinksContainer = document.querySelector('.drinks-container');
+let snacksContainer = document.querySelector('.snacks-container')
+let dessertsContainer = document.querySelector('.desserts-container')
+
